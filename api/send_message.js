d6c8c5ec729b821500ca57236a1c67e914a32df9
Maxwell Fite <maxwellfite@users.noreply.github.com>
2024-06-09 07:07:23 -0700

    Update send_message.js

--- api/send_message.js
+++ api/send_message.js
@@ -1,3 +1,8 @@
 export default (req, res) => {
-    res.status(200).json({ status: 'Message sent!' });
+    const { message } = req.body;
+    if (message) {
+        res.status(200).json({ status: `Message sent: ${message}` });
+    } else {
+        res.status(400).json({ status: 'No message provided' });
+    }
 };
